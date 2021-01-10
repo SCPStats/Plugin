817291215eb9cb0fc10bac413ef1e883dd850641
PintTheDragon <64887885+PintTheDragon@users.noreply.github.com>
2021-01-10 12:53:37 -0800

    Update AssemblyInfo.cs

--- SCPStats/Properties/AssemblyInfo.cs
+++ SCPStats/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PintTheDragon")]
 [assembly: AssemblyProduct("SCPStats")]
-[assembly: AssemblyCopyright("Copyright © PintTheDragon 2020")]
+[assembly: AssemblyCopyright("Copyright © PintTheDragon 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
