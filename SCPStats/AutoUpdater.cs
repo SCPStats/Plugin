312c8cae2f9d7087d71dfff451fbe77e3101938d
PintTheDragon <PintTheDragon@hotmail.com>
2021-01-31 16:37:21 -0800

    Update version

--- SCPStats/AutoUpdater.cs
+++ SCPStats/AutoUpdater.cs
@@ -10,7 +10,7 @@ namespace SCPStats
 {
     internal static class AutoUpdater
     {
-        private const string Version = "1.2.0";
+        private const string Version = "1.2.0-1";
 
         internal static async Task RunUpdater(int waitTime = 0)
         {
