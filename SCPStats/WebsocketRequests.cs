5ab6dcd4012c341c2b10aba529fe522e037613e4
PintTheDragon <PintTheDragon@hotmail.com>
2021-02-16 03:26:24 -0800

    Include seconds

--- SCPStats/WebsocketRequests.cs
+++ SCPStats/WebsocketRequests.cs
@@ -55,7 +55,7 @@ namespace SCPStats
 
                 if (warningSplit.Length < 4) continue;
 
-                result += warningSplit[0] + (warningSplit[3] != SCPStats.Singleton?.Config?.ServerId ? "*" : "") + " | " + WarningTypes[warningSplit[1]] + " | " + warningSplit[2] + "\n" + (warningSplit.Length > 4 && warningSplit[1] == "1" ? " | " + warningSplit[4] : "");
+                result += warningSplit[0] + (warningSplit[3] != SCPStats.Singleton?.Config?.ServerId ? "*" : "") + " | " + WarningTypes[warningSplit[1]] + " | " + warningSplit[2] + "\n" + (warningSplit.Length > 4 && warningSplit[1] == "1" ? " | " + warningSplit[4] + " seconds" : "");
             }
 
             result += "\n*=Warning was not made in this server.";
