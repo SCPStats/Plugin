4f75b3da541c7d9811d99237e967796e23b28be7
PintTheDragon <PintTheDragon@hotmail.com>
2021-03-05 12:26:03 -0800

    Bump version

--- SCPStats/AutoUpdater.cs
+++ SCPStats/AutoUpdater.cs
@@ -7,7 +7,7 @@ namespace SCPStats
 {
     internal static class AutoUpdater
     {
-        private const string Version = "1.2.3-4";
+        private const string Version = "1.2.3-5";
 
         internal static async Task RunUpdater(int waitTime = 0)
         {
