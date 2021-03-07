5b7fea5d5e1be0351ee380303ec4b1e407d08487
PintTheDragon <PintTheDragon@hotmail.com>
2021-03-07 13:20:27 -0800

    Update to latest EXILED

--- SCPStats/SCPStats.cs
+++ SCPStats/SCPStats.cs
@@ -16,7 +16,7 @@ namespace SCPStats.Commands
         public override string Name { get; } = "SCPStats";
         public override string Author { get; } = "PintTheDragon";
         public override Version Version { get; } = new Version(1, 2, 3);
-        public override Version RequiredExiledVersion { get; } = new Version(2, 6, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 8, 0);
         public override PluginPriority Priority { get; } = PluginPriority.Last;
         public override string Prefix { get; } = "scp_stats";
 
