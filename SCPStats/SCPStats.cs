cf5aefc7c35a6b8cbe99bb925e96e88cdb6b190e
PintTheDragon <PintTheDragon@hotmail.com>
2021-03-09 00:16:54 -0800

    Bump version

--- SCPStats/SCPStats.cs
+++ SCPStats/SCPStats.cs
@@ -15,7 +15,7 @@ namespace SCPStats
     {
         public override string Name { get; } = "SCPStats";
         public override string Author { get; } = "PintTheDragon";
-        public override Version Version { get; } = new Version(1, 2, 3);
+        public override Version Version { get; } = new Version(1, 2, 4);
         public override Version RequiredExiledVersion { get; } = new Version(2, 8, 0);
         public override PluginPriority Priority { get; } = PluginPriority.Last;
         public override string Prefix { get; } = "scp_stats";
