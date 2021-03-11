81178544861f6bd74a185642237b0de8114a2ab7
PintTheDragon <PintTheDragon@hotmail.com>
2021-03-10 21:17:13 -0800

    Remove hat from player on disconnect

--- SCPStats/EventHandler.cs
+++ SCPStats/EventHandler.cs
@@ -343,6 +343,7 @@ namespace SCPStats
             WebsocketHandler.SendRequest(RequestType.Leave, "{\"playerid\": \""+playerInfo.PlayerID+"\"}");
 
             if (Players.Contains(ev.Player.UserId)) Players.Remove(ev.Player.UserId);
+            if (HatCommand.HatPlayers.ContainsKey(ev.Player.UserId)) HatCommand.HatPlayers.Remove(ev.Player.UserId);
         }
 
         internal static void OnUse(DequippedMedicalItemEventArgs ev)
