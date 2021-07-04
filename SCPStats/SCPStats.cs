a9f8aac65acd5d65b21de7649d2c3a55fdd06dbe
PintTheDragon <PintTheDragon@hotmail.com>
2021-07-03 21:41:35 -0700

    Bump required EXILED version

--- SCPStats/SCPStats.cs
+++ SCPStats/SCPStats.cs
@@ -23,7 +23,7 @@ namespace SCPStats
         public override string Name { get; } = "SCPStats";
         public override string Author { get; } = "PintTheDragon";
         public override Version Version { get; } = new Version(1, 3, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(2, 8, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 11, 0);
         public override PluginPriority Priority { get; } = PluginPriority.Last;
         public override string Prefix { get; } = "scp_stats";
 
