349140db4057ac2b6b35131071ef6158cedf9ea6
PintTheDragon <PintTheDragon@hotmail.com>
2021-07-14 20:42:57 -0700

    Add file header

--- SCPStats/Patches/BanFilePatch.cs
+++ SCPStats/Patches/BanFilePatch.cs
@@ -1,4 +1,11 @@
-﻿using HarmonyLib;
+﻿// -----------------------------------------------------------------------
+// <copyright file="BanFilePatch.cs" company="SCPStats.com">
+// Copyright (c) SCPStats.com. All rights reserved.
+// Licensed under the Apache v2 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using HarmonyLib;
 
 namespace SCPStats.Patches
 {
