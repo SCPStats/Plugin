b614bc57a4fb5ea5439b71ca8015f09cf142a015
PintTheDragon <PintTheDragon@hotmail.com>
2021-08-21 14:04:17 -0700

    Bump version in auto updater

--- SCPStats/AutoUpdater.cs
+++ SCPStats/AutoUpdater.cs
@@ -14,7 +14,7 @@ namespace SCPStats
 {
     internal static class AutoUpdater
     {
-        private const string Version = "1.4.0-beta1";
+        private const string Version = "1.4.0-beta2";
         private const string Channel = "2";
 
         internal static async Task RunUpdater(int waitTime = 0)
