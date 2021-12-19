59608af6ae402cf1889b859f0de42f640b8c6773
PintTheDragon <PintTheDragon@hotmail.com>
2021-12-18 23:43:29 -0800

    Stop hats from running actions on collision

---
+++ SCPStats/Patches/CollisionPickupPatch.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CollisionPickupPatch.cs" company="SCPStats.com">
+// Copyright (c) SCPStats.com. All rights reserved.
+// Licensed under the Apache v2 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using HarmonyLib;
+using InventorySystem.Items.Pickups;
+using InventorySystem.Items.ThrowableProjectiles;
+using SCPStats.Hats;
+
+namespace SCPStats.Patches
+{
+    [HarmonyPatch(typeof(CollisionDetectionPickup), nameof(CollisionDetectionPickup.OnCollisionEnter))]
+    public class CollisionPickupPatch
+    {
+        public static bool Prefix(ThrownProjectile __instance)
+        {
+            if (__instance.gameObject.TryGetComponent<HatItemComponent>(out _)) return false;
+            return true;
+        }
+    }
+}
