19daaec116b9ca9151cb1b7ef9146bab95f913c2
PintTheDragon <PiintTheDragon@hotmail.com>
2022-04-12 11:59:54 -0700

    Update to 11.2

--- SCPStats/AutoUpdater.cs
+++ SCPStats/AutoUpdater.cs
@@ -14,8 +14,8 @@ namespace SCPStats
 {
     internal static class AutoUpdater
     {
-        private const string Version = "1.5.2";
-        private const string Channel = "2";
+        private const string Version = "1.5.3";
+        private const string Channel = "1";
 
         internal static async Task RunUpdater(int waitTime = 0)
         {
