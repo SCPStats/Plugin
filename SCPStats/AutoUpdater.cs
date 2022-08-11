065b074a7f1b5896010b7ec69a10cfa2d1b53aa5
PintTheDragon <PintTheDragon@hotmail.com>
2022-08-11 02:35:01 -0700

    Prevent updater from writing empty DLLs

--- SCPStats/AutoUpdater.cs
+++ SCPStats/AutoUpdater.cs
@@ -5,6 +5,8 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using Exiled.API.Features;
@@ -19,23 +21,49 @@ namespace SCPStats
         internal static async Task RunUpdater(int waitTime = 0)
         {
             if (waitTime != 0) await Task.Delay(waitTime);
-            
-            using (var client = new WebClient())
+
+            try
             {
-                if (SCPStats.Singleton == null) return;
-
-                var latestVersion = await client.DownloadStringTaskAsync("https://scpstats.com/update/version/" + Loader.Version.ToString());
-                if (latestVersion == "-1" || latestVersion == Version) return;
-
-                var location = SCPStats.Singleton?.GetPath();
-                if (location == null)
+                using (var client = new WebClient())
                 {
-                    Log.Warn("SCPStats auto updater couldn't determine the plugin path. Make sure your plugin dll is named \"SCPStats.dll\".");
-                    return;
-                }
+                    if (SCPStats.Singleton == null) return;
 
-                await client.DownloadFileTaskAsync("https://github.com/SCPStats/Plugin/releases/download/"+latestVersion.Split('-')[0].Replace("/", "")+"/SCPStats.dll", location);
-                Log.Info("Updated SCPStats. Please restart your server to complete the update.");
+                    var latestVersion =
+                        await client.DownloadStringTaskAsync("https://scpstats.com/update/version/" +
+                                                             Loader.Version.ToString());
+                    if (latestVersion == "-1" || latestVersion == Version) return;
+
+                    var location = SCPStats.Singleton?.GetPath();
+                    if (location == null)
+                    {
+                        Log.Warn(
+                            "SCPStats auto updater couldn't determine the plugin path. Make sure your plugin dll is named \"SCPStats.dll\".");
+                        return;
+                    }
+
+                    var data = await client.DownloadDataTaskAsync(
+                        "https://github.com/SCPStats/Plugin/releases/download/" +
+                        latestVersion.Split('-')[0].Replace("/", "") + "/SCPStats.dll");
+
+                    //This data is expected to be > 50,000 bytes, so we should only save if it is.
+                    if (data.Length > 50000)
+                    {
+                        using (var fs = new FileStream(location, FileMode.Create, FileAccess.Write))
+                        {
+                            fs.Write(data, 0, data.Length);
+                        }
+                        
+                        Log.Info("Updated SCPStats. Please restart your server to complete the update.");
+                    }
+                    else
+                    {
+                        Log.Warn("Received invalid data while trying to download an update.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
             }
         }
     }
