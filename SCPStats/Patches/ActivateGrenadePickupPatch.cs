c664a41d877cd0b07db709317e792842a51a35a0
PintTheDragon <PintTheDragon@hotmail.com>
2022-09-07 20:49:56 -0700

    Patch out hat explosion for new grenades

---
+++ SCPStats/Patches/ActivateGrenadePickupPatch.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ActivateGrenadePickupPatch.cs" company="SCPStats.com">
+// Copyright (c) SCPStats.com. All rights reserved.
+// Licensed under the Apache v2 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Numerics;
+using Footprinting;
+using HarmonyLib;
+using InventorySystem.Items.ThrowableProjectiles;
+using SCPStats.Hats;
+
+namespace SCPStats.Patches
+{
+    [HarmonyPatch(typeof(TimedGrenadePickup), nameof(TimedGrenadePickup.OnExplosionDetected))]
+    public class ActivateGrenadePickupPatch
+    {
+        public static bool Prefix(TimedGrenadePickup __instance, Footprint attacker, Vector3 source, float range)
+        {
+            if (__instance.gameObject.TryGetComponent<HatItemComponent>(out _)) return false;
+            return true;
+        }
+    }
+}
