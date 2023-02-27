b704db1d7bc12dad5244367047fe499d33ddf258
PintTheDragon <PintTheDragon@hotmail.com>
2023-02-26 16:11:01 -0800

    Add debug to RoundStatsData parsing

--- SCPStats/Websocket/Data/RoundStatsData.cs
+++ SCPStats/Websocket/Data/RoundStatsData.cs
@@ -5,6 +5,7 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -43,36 +44,46 @@ namespace SCPStats.Websocket.Data
 
         public RoundStatsData(string data)
         {
-            var parts = data.Split(',');
+            try
+            {
+                var parts = data.Split(',');
 
-            PlayerStats = new Dictionary<Player, Stats>();
+                PlayerStats = new Dictionary<Player, Stats>();
 
-            var players = Player.List.Where(pl => !Helper.IsPlayerNPC(pl) && !pl.ReferenceHub.isLocalPlayer && pl.IsVerified).ToDictionary(Helper.HandleId, pl => pl);
-            
-            KillsByScore = CreateScoreArray(parts[0], "Kills", players);
-            PlayerKillsByScore = CreateScoreArray(parts[1], "PlayerKills", players);
-            ScpKillsByScore = CreateScoreArray(parts[2], "ScpKills", players);
-            KillsByOrder = IDsToPlayers(parts[3].Split('|'), players);
-            PlayerKillsByOrder = IDsToPlayers(parts[4].Split('|'), players);
-            ScpKillsByOrder = IDsToPlayers(parts[5].Split('|'), players);
-            
-            DeathsByScore = CreateScoreArray(parts[6], "Deaths", players);
-            DeathsByOrder = IDsToPlayers(parts[7].Split('|'), players);
-            
-            EscapesByScore = CreateScoreArray(parts[8], "Escapes", players);
-            FastestEscapeByScore = CreateScoreArray(parts[9], "FastestEscape", players);
-            EscapesByOrder = IDsToPlayers(parts[10].Split('|'), players);
+                var players = Player.List
+                    .Where(pl => !Helper.IsPlayerNPC(pl) && !pl.ReferenceHub.isLocalPlayer && pl.IsVerified)
+                    .ToDictionary(Helper.HandleId, pl => pl);
 
-            SodasByScore = CreateScoreArray(parts[11], "Sodas", players);
-            MedkitsByScore = CreateScoreArray(parts[12], "Medkits", players);
-            BallsByScore = CreateScoreArray(parts[13], "Balls", players);
-            AdrenalineByScore = CreateScoreArray(parts[14], "Adrenaline", players);
-            SodasByOrder = IDsToPlayers(parts[15].Split('|'), players);
-            MedkitsByOrder = IDsToPlayers(parts[16].Split('|'), players);
-            BallsByOrder = IDsToPlayers(parts[17].Split('|'), players);
-            AdrenalineByOrder = IDsToPlayers(parts[18].Split('|'), players);
-            
-            XpByScore = CreateScoreArray(parts[19], "Xp", players);
+                KillsByScore = CreateScoreArray(parts[0], "Kills", players);
+                PlayerKillsByScore = CreateScoreArray(parts[1], "PlayerKills", players);
+                ScpKillsByScore = CreateScoreArray(parts[2], "ScpKills", players);
+                KillsByOrder = IDsToPlayers(parts[3].Split('|'), players);
+                PlayerKillsByOrder = IDsToPlayers(parts[4].Split('|'), players);
+                ScpKillsByOrder = IDsToPlayers(parts[5].Split('|'), players);
+
+                DeathsByScore = CreateScoreArray(parts[6], "Deaths", players);
+                DeathsByOrder = IDsToPlayers(parts[7].Split('|'), players);
+
+                EscapesByScore = CreateScoreArray(parts[8], "Escapes", players);
+                FastestEscapeByScore = CreateScoreArray(parts[9], "FastestEscape", players);
+                EscapesByOrder = IDsToPlayers(parts[10].Split('|'), players);
+
+                SodasByScore = CreateScoreArray(parts[11], "Sodas", players);
+                MedkitsByScore = CreateScoreArray(parts[12], "Medkits", players);
+                BallsByScore = CreateScoreArray(parts[13], "Balls", players);
+                AdrenalineByScore = CreateScoreArray(parts[14], "Adrenaline", players);
+                SodasByOrder = IDsToPlayers(parts[15].Split('|'), players);
+                MedkitsByOrder = IDsToPlayers(parts[16].Split('|'), players);
+                BallsByOrder = IDsToPlayers(parts[17].Split('|'), players);
+                AdrenalineByOrder = IDsToPlayers(parts[18].Split('|'), players);
+
+                XpByScore = CreateScoreArray(parts[19], "Xp", players);
+            }
+            catch (Exception e)
+            {
+                Log.Error("An error occurred while parsing RoundStatsData. Here's the details: \"" + data + "\".");
+                throw e;
+            }
         }
 
         private Player[] CreateScoreArray(string data, string propName, Dictionary<string, Player> playersDict)
@@ -97,17 +108,25 @@ namespace SCPStats.Websocket.Data
 
         private Player ParseScore(string data, string propName, Dictionary<string, Player> playersDict)
         {
-            var parts = data.Split(';');
+            try
+            {
+                var parts = data.Split(';');
 
-            var user = parts[0];
-            if(!playersDict.TryGetValue(user, out var player)) return null;
-            
-            var score = int.Parse(parts[1], NumberStyles.Integer, Helper.UsCulture);
-            
-            if(!PlayerStats.ContainsKey(player)) PlayerStats[player] = new Stats();
-            typeof(Stats).GetField(propName)?.SetValue(PlayerStats[player], score);
+                var user = parts[0];
+                if (!playersDict.TryGetValue(user, out var player)) return null;
 
-            return player;
+                var score = int.Parse(parts[1], NumberStyles.Integer, Helper.UsCulture);
+
+                if (!PlayerStats.ContainsKey(player)) PlayerStats[player] = new Stats();
+                typeof(Stats).GetField(propName)?.SetValue(PlayerStats[player], score);
+
+                return player;
+            }
+            catch (Exception e)
+            {
+                Log.Error("An error occurred while parsing a score. Here's the details: \"" + data + "\", \"" + propName + "\".");
+                throw e;
+            }
         }
     }
 }
